aa09f5331f8f3a6b0d37a9613ca992b3a668537e
Jon Shelley <jshelley.menan@gmail.com>
2019-10-25 15:02:22 -0600

    Updated installer file

--- apps/fluent/install_fluent.sh
+++ apps/fluent/install_fluent.sh
@@ -4,6 +4,7 @@
 APP_INSTALL_DIR=${APP_INSTALL_DIR:-/apps}
 TMP_DIR=${TMP_DIR:-/mnt/resource}
 FLUENT_INSTALLER_FILE=${FLUENT_INSTALLER_FILE:-/mnt/resource/FLUIDS_2019R2_LINX64.tar}
+FLUENT_VERSION=${FLUENT_VERSION:-v194}
 
 if [ ! -e $FLUENT_INSTALLER_FILE ]; then
     echo "Error:  $FLUENT_INSTALLER_FILE does not exist"
@@ -27,7 +28,7 @@ yum -y install freetype motif.x86_64 mesa-libGLU-9.0.0-4.el7.x86_64
 ./INSTALL -silent -install_dir $APP_INSTALL_DIR/ansys_inc -fluent -nohelp -disablerss
 
 # fixes required for fluent 19.3.0 on Hb/Hc VMs
-sed -i 's/OMPI_MCA_mca_component_show_load_errors/OMPI_MCA_mca_base_component_show_load_errors/g;s/my_ic_flag="--mca btl self,vader,openib"/#my_ic_flag="--mca btl self,vader,openib"/g' $APP_INSTALL_DIR/ansys_inc/v193/fluent/fluent19.3.0/multiport/mpi_wrapper/bin/mpirun.fl
+sed -i 's/OMPI_MCA_mca_component_show_load_errors/OMPI_MCA_mca_base_component_show_load_errors/g;s/my_ic_flag="--mca btl self,vader,openib"/#my_ic_flag="--mca btl self,vader,openib"/g' $APP_INSTALL_DIR/ansys_inc/$FLUENT_VERSION/fluent/fluent*/multiport/mpi_wrapper/bin/mpirun.fl
 
 cd -
 rm -rf $tmp_dir
